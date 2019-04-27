e2b2a88cf419ced92e61720e127600207031df84
ZiaoLIN <ziaolin@g.harvard.edu>
2019-04-27 17:01:39 -0400

    Update PartialModel.py

--- code/PartialModel.py
+++ code/PartialModel.py
@@ -38,20 +38,31 @@ from multiprocessing import Process, Lock
 
 
 class Partial_Model:
-    def __init__(self, state_dict, capacity):
+    def __init__(self, gpu_index, capacity, global_model):
         """
         capacity = num of local models
         """
+        self.gpu = gpu_index #which gpu this Partial_Model is located
+        self.state_dict = global_model.state_dict # weights of partial model
         self.capacity = capacity # how many local models specified in the same GPU
-        self.state_dict = state_dict # weights of partial model
+        self.global_model = global_model
+        self.true_global = self.global_model.state_dict
+        self.state_dict.to(gpu_index)
 
-
+        self.local_model = Net()
+        self.local_model_list = [self.local_model] * self.capacity
+        
     def partial_updates_sum(self, w_in):
         #w_in represents weights from a local model
         for k in self.state_dict.keys(): # iterate every weight element
             self.state_dict[k] += w_in[k]
         
         return self.state_dict
+
+    
+    def pull_global(self):
+        assert self.global_model.incre_counter == 0   # make sure that global is the true global
+        self.true_global = self.global_model.state_dict
 
 
 def update_partial_sum(l, user_list_, partial_model_):
@@ -69,10 +80,10 @@ if __name__ == '__main__':
     from Lenet import Net
     from multiprocessing import Process, Lock
     
-    init_dict = torch.zeros(5, 3, dtype=torch.long)
-    partial_model=Partial_Model(state_dict = init_dict, capacity = num_of_local)
+    #init_dict = torch.zeros(5, 3, dtype=torch.long)
     number_of_local = k
-    model_list=[]
+    gpu = 1
+    partial_model=Partial_Model(gpu_index = gpu, capacity = num_of_local, global_model = global_model)
     lock = Lock()
     for i in range(k):
         #read in user
@@ -83,7 +94,7 @@ if __name__ == '__main__':
         Process(target=update_partial_sum, args=(lock, user_list, partial_model)).start()
 
     for k in partial_model.state_dict.keys():
-        partial_model.state_dict[k] = torch.div(partial_model.state_dict[k], partial_model.capacity)
+        partial_model.local_model[k] = torch.div(partial_model.state_dict[k], partial_model.capacity)
 
 #update to global model
 
