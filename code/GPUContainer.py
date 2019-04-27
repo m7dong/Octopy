dcb2f6cded9e4a408d2992da5f399dd14c9065bd
Simon Xin Dong <xindong@g.harvard.edu>
2019-04-27 17:14:58 -0400

    Update GPUContainer.py

--- code/GPUContainer.py
+++ code/GPUContainer.py
@@ -1,21 +1,24 @@
 from PartialModel import Partial_Model 
-
+from funcs import chunkIt
+from Users import User
+from Lenet import Net
 
 class GPU_Container:
-    def __init__(self, partial_model, users=[]):
-    	self.partial_model = partial_model
-    	self.users = users
-
-    def add_user(user):
-    	self.users.append(user)
+    def __init__(self, users, global_model, gpu_parallel, device):
+        self.users = users
+    	self.partial_model = Partial_Model(capacity = len(self.users), global_model = global_model, device = device)
+        self.gpu_parallel = gpu_parallel
+        self.device = device
         
+        self.split_for_processing()
         
-# From Xin
-class GPU_Container:
-    def __init__(self, users=[]):
-    	self.partial_model = Partial_Model()
-    	self.users = users
-        self.gpu
-
-    def add_user(user):
-    	self.users.append(user)
+    def split_for_processings(self):
+        self.user_list_for_processings = chunkIt(self.users, self.gpu_parallel)
+        
+    def lunch_one_processing(self, processing_index):
+        ready_model = Net().load_state_dict(self.partial_model.true_global).to(self.device)
+        for user_index in self.user_list_for_processings[processing_index]:
+            current_user = User(user_index=user_index, ready_model=ready_model)
+            current_user.local_train()
+            #TODO: how to push local model (subprocessing N) to partial global (main processing)
+        
