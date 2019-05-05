b3dac3cdcb0eac49740c9ef4d8d0f1851c219c3b
m7dong <mengdong03@hotmail.com>
2019-05-05 01:14:13 -0400

    move methods into class

--- code/GPUContainer.py
+++ code/GPUContainer.py
@@ -7,66 +7,6 @@ import copy
 import time
 import torch
 
-def step_training(processing_index, true_global, device, 
-                            user_list_for_processings, local_model_queue, config, done, step):
-    print("launch local model training process: ", device, processing_index)
-    #print('true global', true_global)
-    ready_model = models.__dict__[config.model]()
-    ready_model.to(device).load_state_dict(true_global)
-    for user_index in user_list_for_processings[processing_index]:
-        ready_model.load_state_dict(true_global)
-        current_user = User(user_index=user_index, ready_model=ready_model, local_epoch=config.num_epochs)
-        current_user.local_train(step, config.num_steps)
-        #print("Ending local model training for user: ", device, processing_index, user_index)
-        local_model_queue.put(copy.deepcopy(current_user.net.state_dict()), block=True)
-        #print("result in local queue for user: ", device, processing_index, user_index)
-    print("Ending local model training process: ", device, processing_index)
-    print("**Ending local model training process: ", device, processing_index)
-    done.clear()
-
-def launch_one_training_process(gpu_index, processing_index, true_global, device, 
-                            user_list_for_processings, local_model_queue, config, done, flags):
-    processing_index_global = (config.num_local_models_per_gpu + 1) * gpu_index + processing_index
-    while True:
-        if flags[processing_index_global] < config.num_steps:
-            print(gpu_index, processing_index, flags[processing_index_global])
-            step = int(flags[processing_index_global].data.tolist()) + 1 
-            step_training(processing_index, true_global, device, 
-                            user_list_for_processings, local_model_queue, config, done, step)
-            flags[processing_index_global] += 1
-            #print('wait', gpu_index, processing_index)
-            #print(flags[processing_index_global])
-            done.wait()
-            #print('resume', gpu_index, processing_index)
-        else:
-            break
-    #print("close training process", gpu_index, processing_index)
-    
-
-def launch_partial_update_process(gpu_index, processing_index, global_queue, local_model_queue, partial_model, config, done, flags):
-    processing_index_global = (config.num_local_models_per_gpu + 1) * gpu_index + processing_index
-    while True:   # scan the queue
-        if not local_model_queue.empty():
-            #print("Processing local queue")  
-            local_model = local_model_queue.get(block=False)            # get a trained local model from the queue
-            flag = partial_model.partial_updates_sum(w_in=local_model)  # add it to partial model
-            if flag == 1:
-                flags[processing_index_global] += 1                     # if enough number of local models are added to partial model
-                global_queue.put(move_to_device(copy.deepcopy(partial_model.state_dict),
-                              torch.device('cpu')), block=True)
-                #print('wait', gpu_index, processing_index)
-                #print(flags[processing_index_global])
-                done.wait()
-                #print('resume', gpu_index, processing_index)
-                partial_model.counter = 0
-        else: 
-            time.sleep(1)  
-        if flags[processing_index_global] >= config.num_steps:
-            break
-    done.wait()
-    #print("close partial model updating process")
-
-
 class GPU_Container:
     def __init__(self, device, config, queue, flags):
         self.users = None
@@ -102,20 +42,67 @@ class GPU_Container:
         self.partial_model = Partial_Model(self.device, len(self.user_list_for_processings), self.true_global, self.config)
 
 
+    def step_training(self, processing_index, step):
+        print("launch local model training process: ", device, processing_index)
+        
+        ready_model = models.__dict__[self.config.model]()
+        ready_model.to(self.device).load_state_dict(self.true_global)
+        for user_index in self.user_list_for_processings[processing_index]:
+            ready_model.load_state_dict(self.true_global)
+            current_user = User(user_index=user_index, ready_model=ready_model, local_epoch=self.config.num_epochs)
+            current_user.local_train(step, self.config.num_steps)
+            self.local_model_queue.put(copy.deepcopy(current_user.net.state_dict()), block=True)
+        
+        print("Ending local model training process: ", self.device, processing_index)
+        self.done.clear()
+
+
+    def launch_one_training_process(self, gpu_index, processing_index):
+        processing_index_global = (self.config.num_local_models_per_gpu + 1) * gpu_index + processing_index
+        
+        while True:
+            if self.flags[processing_index_global] < config.num_steps:
+                print(gpu_index, processing_index, self.flags[processing_index_global])
+                step = int(self.flags[processing_index_global].data.tolist()) + 1 
+                self.step_training(processing_index, step)
+                self.flags[processing_index_global] += 1
+                done.wait()
+            else:
+                break
+    
+
+    def launch_partial_update_process(self, gpu_index, processing_index):
+        processing_index_global = (self.config.num_local_models_per_gpu + 1) * gpu_index + processing_index
+        while True:   # scan the queue
+            if not self.local_model_queue.empty():
+                local_model = self.local_model_queue.get(block=False)            # get a trained local model from the queue
+                flag = self.partial_model.partial_updates_sum(w_in=local_model)  # add it to partial model
+                if flag == 1:
+                    self.flags[processing_index_global] += 1                     # if enough number of local models are added to partial model
+                    self.global_queue.put(move_to_device(copy.deepcopy(self.partial_model.state_dict),
+                                  torch.device('cpu')), block=True)
+                    self.done.wait()
+                    self.partial_model.counter = 0
+            else: 
+                time.sleep(1)  
+            if self.flags[processing_index_global] >= self.config.num_steps:
+                break
+        self.done.wait()
+        #print("close partial model updating process")
+
+
     def launch_gpu(self):
         assert self.done is not None
 
         local_process_list = []
         gpu_index = int(str(self.device)[5:])
         for processing_index in range(self.gpu_parallel):
-            new_p = mp.Process(target=launch_one_training_process, \
-                    args=(gpu_index, processing_index, self.true_global, self.device, self.user_list_for_processings,\
-                            self.local_model_queue, self.config, self.done, self.flags))
+            new_p = mp.Process(target=self.launch_one_training_process, args=(gpu_index, processing_index))
             new_p.start()
             local_process_list.append(new_p)
 
-        global_p = mp.Process(target=launch_partial_update_process, \
-                    args=(gpu_index, self.gpu_parallel, self.global_queue, self.local_model_queue, self.partial_model, self.config, self.done, self.flags))
+        global_p = mp.Process(target=self.launch_partial_update_process, \
+                    args=(gpu_index, self.gpu_parallel))
         global_p.start()
         local_process_list.append(global_p) 
 
